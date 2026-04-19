59df090121ea10c39cc5d0f93fb75f55608330c0
PRIYA BASAVARAJ PANTI <priyabasavarjpanti@gmail.com>
2026-04-19 12:57:34 +0530

    Add files via upload

---
+++ NumericCheck.java
@@ -0,0 +1,41 @@
+package lab3a;
+
+ 
+
+	import java.util.Scanner;
+
+	public class NumericCheck {
+
+	    // User-defined function
+	    public static boolean isNumeric(String str) {
+
+	        if (str == null || str.isEmpty()) {
+	            return false;
+	        }
+
+	        for (int i = 0; i < str.length(); i++) {
+	            if (!Character.isDigit(str.charAt(i))) {
+	                return false;
+	            }
+	        }
+
+	        return true;
+	    }
+
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter a string: ");
+	        String input = sc.nextLine();
+
+	        if (isNumeric(input)) {
+	            System.out.println("The string contains only numeric characters.");
+	        } else {
+	            System.out.println("The string does NOT contain only numeric characters.");
+	        }
+
+	        sc.close();
+	    }
+	}
+ 
